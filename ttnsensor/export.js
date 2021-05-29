e87e4263db18153c27156fbdecd89d6b99374e76
Te0SX <Te0SX@yahoo.gr>
2021-05-29 15:01:08 +0300

    Delete export.js

--- ttnsensor/export.js
+++
@@ -1,8 +0,0 @@
-var fsp = require('fs').promises
-var data = FileSystem.readTextFromFileSync(FileSystem.dataDirectory + "/data.json");
-
-var json_parsed = JSON.parse(data)
-for (var prop in json_parsed) {
-    console.log(json_parsed[prop].info.num_events);
-}
-fsp.writeFile("fileparse.js", json_parsed)
